dbb08bab605ba98156b78ebeab85bcae85b6ad84
Jacob Paccione <jaio0701@gmail.com>
2025-04-02 14:45:09 -0400

    small main menu improvements

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,40 +5,46 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Canvasses
+    public GameObject mainMenuCanvas;
+    public GameObject optionsMenuCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Automatically load WAN scene if the loaded game is master server
         string[] args = System.Environment.GetCommandLineArgs();
-        //ArgDisplay.text = System.Environment.CommandLine;
-        foreach (string a in args)
-        {
-            if (a.StartsWith("PORT_") || a.Contains("MASTER"))
-            {
-                //Load Wan scene.
+        foreach (string a in args) {
+            if (a.StartsWith("PORT_") || a.Contains("MASTER")) {
                 SceneManager.LoadScene(1);
             }
         }
+
+        // Set the main menu active and options menu inactive
+        mainMenuCanvas.SetActive(true);
+        optionsMenuCanvas.SetActive(false);
     }
+
     // Update is called once per frame
-    void Update()
+    void Update() {}
+
+    public void WANConnect() { SceneManager.LoadScene(1); }
+
+    public void LANConnect() { SceneManager.LoadScene(2); }
+
+    public void GoToOptions()
     {
-        
+        mainMenuCanvas.SetActive(false);
+        optionsMenuCanvas.SetActive(true);
     }
 
-    public void WANConnect()
-    {
-        //Load Wan scene.
-        SceneManager.LoadScene(1);
-    }
+    public void QuitMe() { Application.Quit(); }
 
-    public void LANConnect()
-    {
-        //Load Lan Scene;
-        SceneManager.LoadScene(2);
-    }
 
-    public void QuitMe()
+    // OPTIONS MENU BUTTONS
+    public void BackToMenu()
     {
-        Application.Quit();
+        mainMenuCanvas.SetActive(true);
+        optionsMenuCanvas.SetActive(false);
     }
 }
